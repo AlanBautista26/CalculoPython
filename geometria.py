bee0caac8eaede1b6dc98a438c5836287b4e0ad6
Alan Bautista <bautistachavez.1999@gmail.com>
2024-05-18 20:24:29 -0500

    Add files via upload

---
+++ geometria.py
@@ -0,0 +1,21 @@
+import math
+
+class Circulo:
+    def __init__(self, radio):
+        self.radio = radio
+    
+    def calcular_area(self):
+        return math.pi * self.radio ** 2
+    
+    def calcular_perimetro(self):
+        return 2 * math.pi * self.radio
+
+class Cuadrado:
+    def __init__(self, lado):
+        self.lado = lado
+    
+    def calcular_area(self):
+        return self.lado ** 2
+    
+    def calcular_perimetro(self):
+        return 4 * self.lado
